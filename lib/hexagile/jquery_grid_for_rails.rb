42edd19ceb624443fe8424662283c1c3772fdac3
Tim Galeckas <timgaleckas@chromedome-lite.local>
2010-04-20 10:28:10 -0500

    added better javascript generation from 2dc

--- lib/hexagile/jquery_grid_for_rails.rb
+++ lib/hexagile/jquery_grid_for_rails.rb
@@ -1,59 +1,307 @@
 module Hexagile
   module JqueryGridForRails
-    def jquery_grid(name, opts={})
-      @@grid_name = name.to_s
-      grid_data = YAML::load_file("#{RAILS_ROOT}/config/jquery_grid/#{name}.yml")
+    def jquery_grid(id, options={})
+      grid_data = YAML::load_file("#{RAILS_ROOT}/config/jquery_grid/#{id}.yml")
 
-      before = grid_data[:before]
-      params = grid_data[:params]
-      
-      def param_body(value)
-        value.scan(/^\((.+?)\)(.+)/).flatten
+      jqgrid(options[:title]||id.to_s,id.to_s,options[:url],grid_data.delete(:columns),grid_data)
+    end
+
+    def gen_columns(columns)
+      col_names = '['
+      col_model = '['
+      columns.each do |c|
+        col_names << "'#{c.delete(:label)}',"
+        col_model << "{%s}" % c.map{|a,b|"#{a.to_s}:'#{b.to_s}'"} * ', ')
       end
-      
-      jquery_grid_code = <<-CODE
-          <script type="text/javascript">
-            <%= before %>_n_
-        		jQuery(document).ready(function(){ 
-        		  jQuery("#<%= name %>").jqGrid({        		    
-        		  <% params.each do |key,value| %>
-        		    <% if opts.keys.include?(key) %>
-        		      _ss_<%= key %>: <%= opts[key].to_json %>,
-        		    <% elsif (value.is_a? String) && (value[0..0] == "$") %>
-        		      _ss_<%= key %>: <%= "jQuery"+value[1,value.length] %>,
-        		    <% elsif (key.to_s[0..8] == 'function_') %>
-        		      <% js_params, body = param_body(value) %>
-        		      _ss_<%= key.to_s[9..-1] %>: function(<%= js_params %>) {
-        		        _ss__ss_<%= body %> 
-        		      _ss_},
-        		    <% else %>
-        		      _ss_<%= key %>: <%= value.to_json %>,
-        		    <% end %>
-              <% end %>                            
-      		    <% opts.each do |key,value| %>
-      		      <% unless params.keys.include?(key) %>
-      		        _ss_<%= key %>: <%= value.to_json %>,
-      		      <% end %>
-      		    <% end %> });
-        	  });
-          </script>
-      CODE
-      ::ERB.new(jquery_grid_code, nil, '>').result(binding).gsub("_n_", "\n").
-        gsub(/[ \t]{2,}/,' ').
-        gsub(/,\s+\}\);/, "\n  });").
-        gsub(/\n\s*\n/m, "\n").
-        gsub("_ss_", "  ").
-        gsub("<grid_name>", @@grid_name)
+      col_names.chop! << "]"
+      col_model.chop! << "]"
+      [col_names, col_model]
     end
     
-    def jquery_grid_table
-      "<table id=\"#{@@grid_name}\" class=\"scroll\"></table>"
+    def jqgrid(title, id, action, columns = [], options = {})
+
+      # Default options
+      options =
+        {
+          :rows_per_page       => '25',
+          :sort_column         => '',
+          :sort_order          => '',
+          :height              => '500',
+          :gridview            => 'false',
+          :error_handler       => 'null',
+          :inline_edit_handler => 'null',
+          :add                 => 'false',
+          :delete              => 'false',
+          :search              => 'true',
+          :edit                => 'false',
+          :inline_edit         => 'false',
+          :autowidth           => 'false',
+          :rownumbers          => 'false',
+          :cell_edit           => 'false',
+          :cell_url            => ''
+        }.merge(options)
+
+      # Stringify options values
+      options.inject({}) do |options, (key, value)|
+        options[key] = (key != :subgrid) ? value.to_s : value
+        options
+      end
+
+      options[:error_handler_return_value] = (options[:error_handler] == 'null') ? 'true;' : options[:error_handler]
+      edit_button = (options[:edit] == 'true' && options[:inline_edit] == 'false' && options[:cell_edit] == 'false').to_s
+
+      # Generate columns data
+      col_names, col_model = gen_columns(columns)
+
+      # Enable filtering (by default)
+      search = ""
+      filter_toolbar = ""
+      if options[:search] == 'true'
+        search = %Q/.navButtonAdd("##{id}_pager",{caption:"",title:"Toggle Search Toolbar", buttonicon :'ui-icon-search', onClickButton:function(){ mygrid[0].toggleToolbar() } })/
+        filter_toolbar = "mygrid.filterToolbar();"
+        filter_toolbar << "mygrid[0].toggleToolbar()"
+      end
+
+      # Enable multi-selection (checkboxes)
+      multiselect = "multiselect: false,"
+      if options[:multi_selection]
+        multiselect = "multiselect: true,"
+        multihandler = %Q/
+          jQuery("##{id}_select_button").click( function() {
+            var s; s = jQuery("##{id}").getGridParam('selarrrow');
+            #{options[:selection_handler]}(s);
+            return false;
+          });/
+      end
+
+      # Enable master-details
+      masterdetails = ""
+      if options[:master_details]
+        masterdetails = %Q/
+          onSelectRow: function(ids) {
+            if(ids == null) {
+              ids=0;
+              if(jQuery("##{id}_details").getGridParam('records') >0 )
+              {
+                jQuery("##{id}_details").setGridParam({url:"#{options[:details_url]}?q=1&id="+ids,page:1})
+                .setCaption("#{options[:details_caption]}: "+ids)
+                .trigger('reloadGrid');
+              }
+            }
+            else
+            {
+              jQuery("##{id}_details").setGridParam({url:"#{options[:details_url]}?q=1&id="+ids,page:1})
+              .setCaption("#{options[:details_caption]} : "+ids)
+              .trigger('reloadGrid');
+            }
+          },/
+      end
+
+      # Enable selection link, button
+      # The javascript function created by the user (options[:selection_handler]) will be called with the selected row id as a parameter
+      selection_link = ""
+      if options[:direct_selection].blank? && options[:selection_handler].present? && options[:multi_selection].blank?
+        selection_link = %Q/
+        jQuery("##{id}_select_button").click( function(){
+          var id = jQuery("##{id}").getGridParam('selrow');
+          if (id) {
+            #{options[:selection_handler]}(id);
+          } else {
+            alert("Please select a row");
+          }
+          return false;
+        });/
+      end
+
+      # Enable direct selection (when a row in the table is clicked)
+      # The javascript function created by the user (options[:selection_handler]) will be called with the selected row id as a parameter
+      direct_link = ""
+      if options[:direct_selection] && options[:selection_handler].present? && options[:multi_selection].blank?
+        direct_link = %Q/
+        onSelectRow: function(id){
+          if(id){
+            #{options[:selection_handler]}(id);
+          }
+        },/
+      end
+
+      cell_selection_link = ""
+      if options[:direct_selection].blank? && options[:cell_edit] != 'true' && options[:cell_select_handler].present?
+        cell_selection_link = %Q/
+        onCellSelect:  function(rowid, iCol, cellcontent){
+          if (rowid && iCol && cellcontent) {
+            #{options[:cell_select_handler]}(rowid, iCol, cellcontent);
+          }
+        },/
+      end
+
+      # Enable grid_loaded callback
+      # When data are loaded into the grid, call the Javascript function options[:grid_loaded] (defined by the user)
+      grid_loaded = ""
+      if options[:grid_loaded].present?
+        grid_loaded = %Q/
+        loadComplete: function(){
+          #{options[:grid_loaded]}();
+        },
+        /
+      end
+
+      # Enable inline editing
+      # When a row is selected, all fields are transformed to input types
+      editable = ""
+      if options[:edit] && options[:inline_edit] == 'true' && options[:cell_edit] == 'false'
+        editable = %Q/
+        onSelectRow: function(id){
+          if(id && id!==lastsel){
+            jQuery('##{id}').restoreRow(lastsel);
+            jQuery('##{id}').editRow(id, true, #{options[:inline_edit_handler]}, #{options[:error_handler]});
+            lastsel=id;
+          }
+        },/
+      end
+
+      # Enable subgrids
+      subgrid = ""
+      subgrid_enabled = "subGrid:false,"
+
+      if options[:subgrid].present?
+
+        subgrid_enabled = "subGrid:true,"
+
+        options[:subgrid] =
+          {
+            :rows_per_page => '10',
+            :sort_column   => 'id',
+            :sort_order    => 'asc',
+            :add           => 'false',
+            :edit          => 'false',
+            :delete        => 'false',
+            :search        => 'false'
+          }.merge(options[:subgrid])
+
+        # Stringify options values
+        options[:subgrid].inject({}) do |suboptions, (key, value)|
+          suboptions[key] = value.to_s
+          suboptions
+        end
+
+        subgrid_inline_edit = ""
+        if options[:subgrid][:inline_edit] == true
+          options[:subgrid][:edit] = 'false'
+          subgrid_inline_edit = %Q/
+          onSelectRow: function(id){
+            if(id && id!==lastsel){
+              jQuery('#'+subgrid_table_id).restoreRow(lastsel);
+              jQuery('#'+subgrid_table_id).editRow(id,true);
+              lastsel=id;
+            }
+          },
+          /
+        end
+
+        if options[:subgrid][:direct_selection] && options[:subgrid][:selection_handler].present?
+          subgrid_direct_link = %Q/
+          onSelectRow: function(id){
+            if(id){
+              #{options[:subgrid][:selection_handler]}(id);
+            }
+          },
+          /
+        end
+
+        sub_col_names, sub_col_model = gen_columns(options[:subgrid][:columns])
+
+        subgrid = %Q(
+        subGridRowExpanded: function(subgrid_id, row_id) {
+        		var subgrid_table_id, pager_id;
+        		subgrid_table_id = subgrid_id+"_t";
+        		pager_id = "p_"+subgrid_table_id;
+        		$("#"+subgrid_id).html("<table id='"+subgrid_table_id+"' class='scroll'></table><div id='"+pager_id+"' class='scroll'></div>");
+        		jQuery("#"+subgrid_table_id).jqGrid({
+        			url:"#{options[:subgrid][:url]}?q=2&id="+row_id,
+              editurl:'#{options[:subgrid][:edit_url]}?parent_id='+row_id,
+        			datatype: "json",
+        			colNames: #{sub_col_names},
+        			colModel: #{sub_col_model},
+        		   	rowNum:#{options[:subgrid][:rows_per_page]},
+        		   	pager: pager_id,
+        		   	imgpath: '/images/themes/lightness/images',
+        		   	sortname: '#{options[:subgrid][:sort_column]}',
+        		    sortorder: '#{options[:subgrid][:sort_order]}',
+                viewrecords: true,
+                toolbar : [true,"top"],
+        		    #{subgrid_inline_edit}
+        		    #{subgrid_direct_link}
+        		    height: '100%'
+        		})
+        		.navGrid("#"+pager_id,{edit:#{options[:subgrid][:edit]},add:#{options[:subgrid][:add]},del:#{options[:subgrid][:delete]},search:false})
+        		.navButtonAdd("#"+pager_id,{caption:"Search",title:"Toggle Search",buttonimg:'/images/jqgrid/search.png',
+            	onClickButton:function(){
+            		if(jQuery("#t_"+subgrid_table_id).css("display")=="none") {
+            			jQuery("#t_"+subgrid_table_id).css("display","");
+            		} else {
+            			jQuery("#t_"+subgrid_table_id).css("display","none");
+            		}
+            	}
+            });
+            jQuery("#t_"+subgrid_table_id).height(25).hide().filterGrid(""+subgrid_table_id,{gridModel:true,gridToolbar:true});
+        	},
+        	subGridRowColapsed: function(subgrid_id, row_id) {
+        	},
+        )
+      end
+
+      # Generate required Javascript & html to create the jqgrid
+      %Q(
+        <script type="text/javascript">
+        var lastsel;
+        jQuery(document).ready(function(){
+        var mygrid = jQuery("##{id}").jqGrid({
+            url:'#{action}#{action.include?('?') ? '&' : '?'}q=1',
+            editurl:'#{options[:edit_url]}',
+            datatype: "json",
+            colNames:#{col_names},
+            colModel:#{col_model},
+            pager: '##{id}_pager',
+            rowNum:#{options[:rows_per_page]},
+            rowList:[10,25,50,100],
+            imgpath: '/images/themes/lightness/images',
+            sortname: '#{options[:sort_column]}',
+            viewrecords: true,
+            height: #{options[:height]},
+            sortorder: '#{options[:sort_order]}',
+            gridview: #{options[:gridview]},
+            scrollrows: true,
+            autowidth: #{options[:autowidth]},
+            rownumbers: #{options[:rownumbers]},
+            cellEdit: #{options[:cell_edit]},
+            cellurl: '#{options[:cell_url]}',
+            #{multiselect}
+            #{masterdetails}
+            #{grid_loaded}
+            #{direct_link}
+            #{cell_selection_link}
+            #{editable}
+            #{subgrid_enabled}
+            #{subgrid}
+            caption: "#{title}"
+        })
+        .navGrid('##{id}_pager',
+        {edit:#{edit_button},add:#{options[:add]},del:#{options[:delete]},search:false,refresh:true},
+        {afterSubmit:function(r,data){return #{options[:error_handler_return_value]}(r,data,'edit');}},
+        {afterSubmit:function(r,data){return #{options[:error_handler_return_value]}(r,data,'add');}},
+        {afterSubmit:function(r,data){return #{options[:error_handler_return_value]}(r,data,'delete');}})
+        #{search}
+        #{multihandler}
+        #{selection_link}
+        #{filter_toolbar}
+        });
+        </script>
+        <table id="#{id}" class="scroll" cellpadding="0" cellspacing="0"></table>
+        <div id="#{id}_pager" class="scroll" style="text-align:center; width: 992px"></div>
+      )
     end
-    
-    def jquery_grid_pager
-      "<div id=\"#{@@grid_name}_pager\" class=\"scroll\" style=\"text-align:center;\"></div>"
-    end
-    
+
     def include_jquery_grid_javascript
       <<-JS
       <script src="/javascripts/jqGrid/i18n/grid.locale-en.js" type="text/javascript"></script>
@@ -63,7 +311,8 @@ module Hexagile
     
     def include_jquery_grid_css
       <<-CSS
-      <link href="/stylesheets/jqGrid/jquery-ui-1.7.1.custom.css" media="screen" rel="stylesheet" type="text/css" />
+      <link href="/stylesheets/cupertino/jquery-ui-1.7.2.custom.css" media="screen" rel="stylesheet" type="text/css" />
+      <link href="/stylesheets/jqGrid/ui.jqgrid.css" media="screen" rel="stylesheet" type="text/css" />
       CSS
     end
   end
