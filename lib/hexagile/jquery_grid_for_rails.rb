d2965b461746a792ae94651adc8601ea6182fe6d
Tim Galeckas <timgaleckas@chromedome-lite.local>
2010-04-21 00:35:17 -0500

    pagination and search state management

--- lib/hexagile/jquery_grid_for_rails.rb
+++ lib/hexagile/jquery_grid_for_rails.rb
@@ -328,5 +328,74 @@ module Hexagile
       <link href="/stylesheets/jqGrid/ui.jqgrid.css" media="screen" rel="stylesheet" type="text/css" />
       CSS
     end
+    
+    def update_pagination_state_with_params!(restraining_model = nil)
+      model_klass = (restraining_model.is_a?(Class) || restraining_model.nil? ? restraining_model : restraining_model.to_s.classify.constantize)
+      pagination_state = previous_pagination_state(model_klass)
+      limit = (params[:rows] || pagination_state[:limit] || 25).to_i
+      offset = params[:page] ? ((params[:page].to_i - 1)*limit) : (( pagination_state[:offset] || 0).to_i)
+      pagination_state.merge!({
+              :sort_field => (params[:sidx] || pagination_state[:sort_field] || "#{restraining_model.table_name}.id").sub(/(\A[^\[]*)\[([^\]]*)\]/,'\2'), # fields may be passed as "object[attr]"
+              :sort_direction => (params[:sord] || pagination_state[:sort_direction]).to_s.upcase,
+              :offset => offset,
+              :limit => limit
+      })
+      # allow only valid sort_fields matching column names of the given model ...
+      unless model_klass.nil? || model_klass.column_names.include?(pagination_state[:sort_field])
+        pagination_state.delete(:sort_field)
+        pagination_state.delete(:sort_direction)
+      end
+      # ... and valid sort_directions
+      pagination_state.delete(:sort_direction) unless %w(ASC DESC).include?(pagination_state[:sort_direction])
+
+      save_pagination_state(pagination_state, model_klass)
+    end
+
+    def options_from_pagination_state(pagination_state)
+      find_options = { :offset => pagination_state[:offset],
+                       :limit  => pagination_state[:limit] }
+      find_options.merge!(
+              :order => "#{pagination_state[:sort_field]} #{pagination_state[:sort_direction]}"
+      ) unless pagination_state[:sort_field].blank?
+
+      find_options
+    end
+
+    def options_from_search(restraining_model = nil)
+      returning options = {} do
+        model_klass = (restraining_model.is_a?(Class) || restraining_model.nil? ? restraining_model : restraining_model.to_s.classify.constantize)
+        sub_expressions = []
+        values = {}
+        if params["_search".to_sym]
+          model_klass.columns_hash.each do |attribute_name, column|
+            if value = params[attribute_name.to_sym]
+              key = "#{column.table_name}.#{attribute_name}"
+              value_placeholder = ":#{attribute_name}"
+
+              #ix adds case insensitive searching
+              sub_expressions << "REGEXP_LIKE(#{key}, #{value_placeholder}, 'i')"
+              values[attribute_name.to_sym] = value
+             end
+          end
+          unless sub_expressions.empty?
+            query = sub_expressions.join(" AND ")
+            logger.debug "Query #{query.inspect}"
+            options.merge!(:conditions => [query, values])
+          end
+        end
+      end
+    end
+
+    private
+
+    # get pagination state from session
+    def previous_pagination_state(model_klass = nil)
+      session["#{model_klass.to_s.tableize.tr('/','_') if model_klass}_pagination_state"] || {}
+    end
+
+    # save pagination state to session
+    def save_pagination_state(pagination_state, model_klass = nil)
+      session["#{model_klass.to_s.tableize.tr('/','_') if model_klass}_pagination_state"] = pagination_state
+    end
   end
 end
